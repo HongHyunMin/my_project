49444191f8790a2b0413c54443f4f1a2f6310091
honghyunmin <paustzero@naver.com>
2016-11-17 09:44:23 +0900

    ejx setting

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ var express = require('express');
 
 var app = express();
 
+app.set("view engine", 'ejs');
+
 app.use(express.static(__dirname + '/public'));
 
 console.log(__dirname);
 
 app.get('/', function(req,res){
-  res.send('Hello World');
+  res.render('my_first_ejs');
 });
+// app.get('/', function(req,res){
+//   res.send('Hello World');
+// });
 
 app.listen(3000, function(){
   console.log('Server On!');
