77e6dcaed8a6be674850794ac392e47395613380
honghyunmin <paustzero@naver.com>
2016-11-21 13:10:26 +0900

    board setting

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
 
+// import modules
 var express = require('express');
-var path = require('path');
 var app = express();
+var path = require('path');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
+// connect database
 mongoose.connect(process.env.MONGO_DB);
-
 var db = mongoose.connection;
-db.once("open", function(){
+db.once("open",function () {
   console.log("DB connected!");
 });
-db.on("error", function(err){
+db.on("error",function (err) {
   console.log("DB ERROR :", err);
 });
 
+// model setting
 var postSchema = mongoose.Schema({
-  title:      {type: String, required: true},
-  body:       {type: String, required: true},
-  createdAt:  {type: Date, default: Date.now},
-  updateedAt: Date
+  title: {type:String, required:true},
+  body: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now},
+  updatedAt: Date
 });
+var Post = mongoose.model('post',postSchema);
 
-var Post = mongoose.model('post', postSchema);
-
+// view setting
 app.set("view engine", 'ejs');
 
-app.use(express.static(path.join( __dirname + 'public')));
+// set middlewares
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 
+// set routes
+app.get('/', function(req,res){
+  Post.find({}).sort('-createdAt').exec(function (err,posts) {
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/index", {data:posts});
+  });
+}); // index
 // set routes
 app.get('/posts', function(req,res){
   Post.find({}).sort('-createdAt').exec(function (err,posts) {
